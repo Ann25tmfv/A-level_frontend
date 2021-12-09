ae80869f7bb3125356bb03f1b5b0e3c82f759cf1
Ann25@tmfv <kisilanna25@gmail.com>
2021-12-09 10:17:20 +0200

    add script.js

---
+++ js/lecture_5/script.js
@@ -0,0 +1,11 @@
+/**
+Task 3.
+ */
+
+var initialString = 'hello';
+function searchSubstringIndex (initialString, searchingSubstring) {
+return initialString.index0f(searchingSubstring)
+}
+SearchSubstringIndex (initialString, 'world');
+
+// еще думаю 
